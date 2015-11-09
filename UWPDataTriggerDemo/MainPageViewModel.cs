b10918ec401db4d7a9560fb7581d8c4ca26d602a
jef-daels <jef.daels@hotmail.be>
2015-11-09 17:13:22 +0100

    blog version

---
+++ UWPDataTriggerDemo/MainPageViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UWPDataTriggerDemo
+{
+    public class MainPageViewModel : INotifyPropertyChanged {
+        private String _userInput;
+        public String UserInput {
+            get { return _userInput; }
+            set {
+                if (_userInput == value) return;
+                _userInput = value;
+                OnPropertyChanged();
+                OnPropertyChanged(nameof(IsValidInteger));
+            }
+        }
+        public Boolean IsValidInteger
+        {
+            get {
+                int i = 0;
+                return int.TryParse(UserInput, out i);
+            }
+        }
+        #region "propertychanged"
+        public event PropertyChangedEventHandler PropertyChanged;
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = ""){
+            if (PropertyChanged != null) PropertyChanged(this, new PropertyChangedEventArgs(propertyName));
+        }
+        #endregion
+    }
+}
